d2eff1d08e16f0f279136de09e39086e8d703450
Rogério Carvalho Schneider <stockrt@gmail.com>
2015-01-10 22:07:28 -0200

    Static and Shared builds

---
+++ Makefile
@@ -0,0 +1,26 @@
+# http://www.sqlite.org/download.html
+SQLITE_VERSION  ?= 3080704
+SQLITE_BASENAME := sqlite-amalgamation-$(SQLITE_VERSION)
+SQLITE_URL      := http://www.sqlite.org/2014/$(SQLITE_BASENAME).zip
+
+# CLI
+libs/armeabi/sqlite3-static: build/sqlite3.c
+	ndk-build
+
+# Unpack
+build/sqlite3.c: $(SQLITE_BASENAME).zip
+	unzip -oq "$<"
+	rm -rf build
+	mv "$(SQLITE_BASENAME)" build
+	touch "$@"
+
+# Download
+$(SQLITE_BASENAME).zip:
+	wget -N -c "$(SQLITE_URL)"
+
+clean:
+	rm -f "$(SQLITE_BASENAME).zip"
+	rm -rf "$(SQLITE_BASENAME)"
+	rm -rf build
+	rm -rf obj
+	rm -rf libs
---
+++ jni/Android.mk
@@ -0,0 +1,49 @@
+LOCAL_PATH := $(call my-dir)
+
+
+#
+# Statically Linked
+#
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := sqlite3-a
+LOCAL_MODULE_FILENAME   := libsqlite3
+LOCAL_SRC_FILES         := ../build/sqlite3.c
+LOCAL_C_INCLUDES        := ../build
+LOCAL_EXPORT_C_INCLUDES := ../build
+LOCAL_CFLAGS            := -DSQLITE_THREADSAFE=1
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := sqlite3-static-cli
+LOCAL_MODULE_FILENAME   := sqlite3-static
+LOCAL_STATIC_LIBRARIES  := libsqlite3-a
+LOCAL_SRC_FILES         := ../build/shell.c ../build/sqlite3.c
+LOCAL_C_INCLUDES        := ../build
+LOCAL_EXPORT_C_INCLUDES := ../build
+LOCAL_CFLAGS            := -DSQLITE_THREADSAFE=1
+include $(BUILD_EXECUTABLE)
+
+
+#
+# Dynamically Linked
+#
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := sqlite3-so
+LOCAL_MODULE_FILENAME   := libsqlite3
+LOCAL_SRC_FILES         := ../build/sqlite3.c
+LOCAL_C_INCLUDES        := ../build
+LOCAL_EXPORT_C_INCLUDES := ../build
+LOCAL_CFLAGS            := -DSQLITE_THREADSAFE=1
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := sqlite3-dynamic-cli
+LOCAL_MODULE_FILENAME   := sqlite3-dynamic
+LOCAL_SHARED_LIBRARIES  := libsqlite3-so
+LOCAL_SRC_FILES         := ../build/shell.c ../build/sqlite3.c
+LOCAL_C_INCLUDES        := ../build
+LOCAL_EXPORT_C_INCLUDES := ../build
+LOCAL_CFLAGS            := -DSQLITE_THREADSAFE=1
+include $(BUILD_EXECUTABLE)
